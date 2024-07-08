13aa948d884a5fe948224f3b117d2b9b9ffe42d1
Viktor Avelino <viktor.avelino@gmail.com>
2024-07-08 15:05:28 -0400

    link

--- portfolio/src/components/sections/projects.tsx
+++ portfolio/src/components/sections/projects.tsx
@@ -7,33 +7,33 @@ const projects: Array<{
   liveUrl?: string;
   github: string;
 }> = [
+  // {
+  //   name: "Messenger",
+  //   img: "https://talentclick.com/wp-content/uploads/2021/08/placeholder-image.png",
+  //   desc: "This is project of a live messenger using SocketIO and React.",
+  //   // liveUrl: "",
+  //   github: "https://github.com/viktoravelino/messenger",
+  // },
+  // {
+  //   name: "Feed",
+  //   img: "https://raw.githubusercontent.com/viktoravelino/feed-layout/main/src/assets/screenShot.png",
+  //   desc: "This project is a simple feed layout.",
+  //   // liveUrl: "https://viktoravelino.com/feed-layout/",
+  //   github: "https://github.com/viktoravelino/feed-layout",
+  // },
+  // {
+  //   name: "Shopping Cart",
+  //   img: "https://raw.githubusercontent.com/viktoravelino/shopping-cart/main/screenShot.png",
+  //   desc: "This project exemplifies a shopping cart using React and persisting data into localStorage.",
+  //   // liveUrl: "https://shopping-cart.viktoravelino.com/",
+  //   github: "https://github.com/viktoravelino/shopping-cart",
+  // },
   {
-    name: "Messenger",
-    img: "https://talentclick.com/wp-content/uploads/2021/08/placeholder-image.png",
-    desc: "This is project of a live messenger using SocketIO and React.",
-    // liveUrl: "",
-    github: "https://github.com/viktoravelino/messenger",
-  },
-  {
-    name: "Feed",
-    img: "https://raw.githubusercontent.com/viktoravelino/feed-layout/main/src/assets/screenShot.png",
-    desc: "This project is a simple feed layout.",
-    // liveUrl: "https://viktoravelino.com/feed-layout/",
-    github: "https://github.com/viktoravelino/feed-layout",
-  },
-  {
-    name: "Shopping Cart",
-    img: "https://raw.githubusercontent.com/viktoravelino/shopping-cart/main/screenShot.png",
-    desc: "This project exemplifies a shopping cart using React and persisting data into localStorage.",
-    // liveUrl: "https://shopping-cart.viktoravelino.com/",
-    github: "https://github.com/viktoravelino/shopping-cart",
-  },
-  {
-    name: "Postman Clone",
+    name: "API Client",
     img: "https://raw.githubusercontent.com/viktoravelino/postman-clone/main/src/ScreenShot.png",
-    desc: "This project clones Postman's functionalities!",
-    // liveUrl: "https://viktoravelino.com/postman-clone/",
-    github: "https://github.com/viktoravelino/postman-clone",
+    desc: "API Client is a simple API client built with React and Axios.",
+    liveUrl: "https://api-client.viktoravelino.com/",
+    github: "https://github.com/viktoravelino/portfolio/tree/dev/api-client",
   },
   {
     name: "Budget Tracker",
